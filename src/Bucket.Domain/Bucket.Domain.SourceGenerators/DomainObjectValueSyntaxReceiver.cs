8906f8ae12273710174fadb1b4833b640ff4d578
dotNevereverlie <tamikappaev@yandex.ru>
2023-07-22 17:42:09 +0300

    Add ISyntaxReceiver implementation, DomainObjectValues property and HasObjectValueInterface method

--- src/Bucket.Domain/Bucket.Domain.SourceGenerators/DomainObjectValueSyntaxReceiver.cs
+++ src/Bucket.Domain/Bucket.Domain.SourceGenerators/DomainObjectValueSyntaxReceiver.cs
@@ -1,11 +1,29 @@
 ﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace Bucket.Domain.SourceGenerators;
 
 public class DomainObjectValueSyntaxReceiver : ISyntaxReceiver
 {
+    private const string ReceiverEntityName = "IDomainObjectValue";
+
+    public List<StructDeclarationSyntax> DomainObjectValues { get; } = new();
+    
     public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
     {
-        throw new NotImplementedException();
+        if (syntaxNode is not StructDeclarationSyntax structDeclaration) 
+            return;
+        
+        if (HasObjectValueInterface(structDeclaration, ReceiverEntityName))
+        {
+            DomainObjectValues.Add(structDeclaration);
+        }
+    }
+
+    private bool HasObjectValueInterface(StructDeclarationSyntax syntaxNode, string interfaceName)
+    {
+        var baseTypes = syntaxNode.BaseList?.Types.Select(baseType => baseType);
+
+        return baseTypes.Any(baseType => baseType.ToString() == interfaceName);
     }
 }
