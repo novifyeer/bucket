572df72478106566bc783acf45570f799fe48b90
dotNevereverlie <tamikappaev@yandex.ru>
2023-07-22 17:12:59 +0300

    Add DomainObjectValueGenerator.cs

---
+++ src/Bucket.Domain/Bucket.Domain.SourceGenerators/DomainObjectValueGenerator.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace Bucket.Domain.SourceGenerators;
+
+[Generator]
+public class DomainObjectValueGenerator : ISourceGenerator 
+{
+    public void Initialize(GeneratorInitializationContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Execute(GeneratorExecutionContext context)
+    {
+        throw new NotImplementedException();
+    }
+}
