e45a1a5312fad76ffe394d05c03859166aa2a969
Yuriry Novikov <techcer@yandex.ru>
2023-07-22 16:08:45 +0300

    Implement catalog brand domain object

---
+++ src/Bucket.Domain/Bucket.Domain/Catalog/CatalogBrand/CatalogBrandDomainModel.cs
@@ -0,0 +1,25 @@
+﻿namespace Bucket.Domain.Catalog.CatalogBrand;
+
+public class CatalogBrandDomainModel
+{
+    private CatalogBrandDomainModel(CatalogBrandName name)
+    {
+        SetName(name);
+    }
+
+    public CatalogBrandId BrandId { get; private set; }
+
+    private void SetBrandId(CatalogBrandId brandId)
+    {
+        BrandId = brandId;
+    }
+
+    public CatalogBrandName Name { get; private set; }
+
+    private void SetName(CatalogBrandName name)
+    {
+        Name = name;
+    }
+
+    public static CatalogBrandDomainModel Create(CatalogBrandName name) => new(name);
+}
