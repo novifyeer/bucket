0ac7f25fd8833fbb7d965b2bb704139cfe75278c
Yuriry Novikov <techcer@yandex.ru>
2023-07-22 17:15:30 +0300

    Implement catalog type domain object

---
+++ src/Bucket.Domain/Bucket.Domain/Catalog/CatalogType/CatalogTypeId.cs
@@ -0,0 +1,10 @@
+﻿namespace Bucket.Domain.Catalog.CatalogType;
+
+public readonly record struct CatalogTypeId
+{
+    public int Value { get; init; }
+
+    private CatalogTypeId(int value) => Value = value;
+
+    public static CatalogTypeId Create(int value) => new(value);   
+}
---
+++ src/Bucket.Domain/Bucket.Domain/Catalog/CatalogType/CatalogTypeName.cs
@@ -0,0 +1,10 @@
+﻿namespace Bucket.Domain.Catalog.CatalogType;
+
+public readonly record struct CatalogTypeName
+{
+    public string Value { get; init; }
+
+    private CatalogTypeName(string value) => Value = value;
+    
+    public static CatalogTypeName Create(string value) => new(value);
+}
