7d0cb050f102837e640bf2e11a84d888c9607185
dotNevereverlie <tamikappaev@yandex.ru>
2023-07-22 17:18:20 +0300

    Add DomainObjectValueSyntaxReceiver

---
+++ src/Bucket.Domain/Bucket.Domain.SourceGenerators/DomainObjectValueSyntaxReceiver.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace Bucket.Domain.SourceGenerators;
+
+public class DomainObjectValueSyntaxReceiver : ISyntaxReceiver
+{
+    public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
+    {
+        throw new NotImplementedException();
+    }
+}
