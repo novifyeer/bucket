f5b76d946ecd7ae12c4e216b3f838c2b50e13da3
Yuriy Novikov <techcer@yandex.ru>
2023-07-22 17:16:58 +0300

    Implement catalog type domain object (#7)

---
+++ src/Bucket.Domain/Bucket.Domain.UnitTests/Domain/Catalog/CatalogTypeDomainModelTest.cs
@@ -0,0 +1,22 @@
+﻿using Bucket.Domain.Catalog.CatalogType;
+
+namespace Bucket.Domain.UnitTests.Domain.Catalog;
+
+public class CatalogTypeDomainModelTest
+{
+    public CatalogTypeDomainModelTest () { }
+    
+    [Fact(DisplayName = "Successful catalog type creation with valid data")]
+    public void CreateCatalogType_WithValidData_CatalogTypeCreated()
+    {
+        // Arrange
+        var name = CatalogTypeName.Create("T-Shirt");
+        
+        // Act
+        var catalogTypeDomain = CatalogTypeDomainModel.Create(name);
+        
+        // Assert
+        catalogTypeDomain.Should().NotBeNull();
+        catalogTypeDomain.Name.Value.Should().Be("T-Shirt");
+    }
+}
---
+++ src/Bucket.Domain/Bucket.Domain/Catalog/CatalogType/CatalogTypeDomainModel.cs
@@ -0,0 +1,19 @@
+﻿namespace Bucket.Domain.Catalog.CatalogType;
+
+public class CatalogTypeDomainModel
+{
+    private CatalogTypeDomainModel(CatalogTypeName name)
+    {
+        SetName(name);
+    }
+
+    public CatalogTypeId TypeId { get; private set; }
+
+    private void SetTypeId(CatalogTypeId brandId) => TypeId = brandId;
+
+    public CatalogTypeName Name { get; private set; }
+
+    private void SetName(CatalogTypeName name) => Name = name;
+
+    public static CatalogTypeDomainModel Create(CatalogTypeName name) => new(name);
+}
