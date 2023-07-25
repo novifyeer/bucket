3d4ccd22085500c252b56cf289c88ce1b87a45d7
Yuriry Novikov <techcer@yandex.ru>
2023-07-26 02:25:22 +0300

    implement a strong amount type to use in prices

---
+++ src/Bucket.Domain/Bucket.Domain.UnitTests/Common/AmountTests/AmountUnitTest.cs
@@ -0,0 +1,13 @@
+﻿namespace Bucket.Domain.UnitTests.Common.AmountTests;
+
+public class AmountUnitTest
+{
+    public AmountUnitTest () { }
+    
+    [Theory(DisplayName = "amountUnit enum value should equal with ordered enum value")]
+    [InlineData(AmountUnit.Unit, 0)]
+    public void AmountUnitEnumValueShouldEqualOrderedEnumValue(AmountUnit amountUnit, int code)
+    {
+        amountUnit.Should().HaveValue(code);
+    }
+}
---
+++ src/Bucket.Domain/Bucket.Domain/Common/Amount/Amount.cs
@@ -0,0 +1,27 @@
+﻿namespace Bucket.Domain.Common.Amount;
+
+public readonly record struct Amount
+{
+    public int Value { get; init; }
+    
+    public AmountUnit Unit { get; init; }
+
+    private Amount(int value, AmountUnit unit)
+    {
+        Value = value;
+        Unit = unit;
+    }
+
+    public static Amount operator +(Amount left, Amount right)
+    {
+        CheckUnits(left, right);
+        return new Amount(left.Value + right.Value, left.Unit);
+    }
+    
+    public static Amount Create(int value, AmountUnit unit) => new(value, unit);
+    
+    private static void CheckUnits(Amount left, Amount right)
+    {
+        if (left.Unit != right.Unit) throw new Exception("the left unit must match the right unit");
+    }
+}
