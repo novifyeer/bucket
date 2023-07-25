6f521cee33e90a760a818a85b1482c2c32275f1c
Yuriry Novikov <techcer@yandex.ru>
2023-07-26 02:27:11 +0300

    implement basket item domain object

---
+++ src/Bucket.Domain/Bucket.Domain/Basket/BasketItem/BasketItemDomainModel.cs
@@ -0,0 +1,48 @@
+﻿using Bucket.Domain.Catalog.CatalogItem;
+
+namespace Bucket.Domain.Basket.BasketItem;
+
+public class BasketItemDomainModel
+{
+    private BasketItemDomainModel(
+        BasketItemId basketItemId,
+        CatalogItemId catalogItemId,
+        BasketItemQuantity quantity) : this(catalogItemId, quantity)
+    {
+        SetBasketItemId(basketItemId);
+    }
+
+    private BasketItemDomainModel(
+        CatalogItemId catalogItemId,
+        BasketItemQuantity quantity)
+    {
+        SetCatalogItemId(catalogItemId);
+        SetQuantity(quantity);
+    }
+
+    public BasketItemId BasketItemId { get; private set; }
+
+    private void SetBasketItemId(BasketItemId basketItemId) => BasketItemId = basketItemId;
+
+    public BasketItemQuantity Quantity { get; private set; }
+
+    private void SetQuantity(BasketItemQuantity quantity) => Quantity = quantity;
+
+    public CatalogItemId CatalogItemId { get; private set; }
+
+    private void SetCatalogItemId(CatalogItemId catalogItemId) => CatalogItemId = catalogItemId;
+
+    public void ChangeQuantity(BasketItemQuantity quantity)
+    {
+        Quantity += quantity;
+    }
+
+    public static BasketItemDomainModel Create(
+        CatalogItemId catalogItemId,
+        BasketItemQuantity quantity) => new(catalogItemId, quantity);
+
+    public static BasketItemDomainModel Restore(
+        BasketItemId basketItemId,
+        CatalogItemId catalogItemId,
+        BasketItemQuantity quantity) => new(basketItemId, catalogItemId, quantity);
+}
---
+++ src/Bucket.Domain/Bucket.Domain/Basket/BasketItem/BasketItemId.cs
@@ -0,0 +1,10 @@
+﻿namespace Bucket.Domain.Basket.BasketItem;
+
+public readonly record struct BasketItemId
+{
+    public int Value { get; init; }
+
+    private BasketItemId(int value) => Value = value;
+
+    public static BasketItemId Create(int value) => new(value);      
+}
